069521f3eaa40d02d6c5f89d4ec1974bf4a48e7c
MahnoorAkhund <62069409+MahnoorAkhund@users.noreply.github.com>
2020-03-11 19:38:55 +0300

    Update Resident.java

--- Resident.java
+++ Resident.java
@@ -2,4 +2,28 @@ package people;
 
 public class Resident extends Tenant {
 
+  private int qatarId;
+	private String bankName;
+	private String bankAccountNo;
+	
+	
+	public int getQatarId() {
+		return qatarId;
+	}
+	public void setQatarId(int qatarId) {
+		this.qatarId = qatarId;
+	}
+	public String getBankName() {
+		return bankName;
+	}
+	public void setBankName(String bankName) {
+		this.bankName = bankName;
+	}
+	public String getBankAccountNo() {
+		return bankAccountNo;
+	}
+	public void setBankAccountNo(String bankAccountNo) {
+		this.bankAccountNo = bankAccountNo;
+	}
+  
 }
