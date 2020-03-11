746ef2b34034e707b5b6866da9c73153264dd4bd
Nourhan Ibrahim <ni.easu2017@gmail.com>
2020-03-12 00:53:58 +0300

    Add files via upload

--- Tenant.java
+++ Tenant.java
@@ -1,13 +1,17 @@
+//Name: Nourhan Ibrahim
+//QU ID: 201608687
+
 package people;
 
 public class Tenant {
-	
+	//Instance variables
 	private int tenantId;
 	private String name;
 	private String phone;
 	private String address;
 	private String nationality;
 	
+	//Constructor
 	public Tenant(int tenantId, String name, String phone, String address, String nationality) {
 		super();
 		this.tenantId = tenantId;
@@ -15,38 +19,45 @@ public class Tenant {
 		this.phone = phone;
 		this.address = address;
 		this.nationality = nationality;
-	}
+	} //end of constructor
+	
+	//Getters and setters
 	
 	public int getTenantId() {
 		return tenantId;
-	}
+	} //end of getTenantId
 	public void setTenantId(int tenantId) {
 		this.tenantId = tenantId;
-	}
+	} //end of setTenantId
 	public String getName() {
 		return name;
-	}
+	} //end of getName
 	public void setName(String name) {
 		this.name = name;
-	}
+	} //end of setName
 	public String getPhone() {
 		return phone;
-	}
+	} //end of getPhone
 	public void setPhone(String phone) {
 		this.phone = phone;
-	}
+	} //end of setName 
 	public String getAddress() {
 		return address;
-	}
+	} //end of getAddress
 	public void setAddress(String address) {
 		this.address = address;
-	}
+	} //end of setAddress
 	public String getNationality() {
 		return nationality;
-	}
+	} //end of getNationality
 	public void setNationality(String nationality) {
 		this.nationality = nationality;
-	}
+	} //end of setNationality
+
+	@Override
+	public String toString() {
+		return "Tenant [tenantId=" + tenantId + ", name=" + name + ", phone=" + phone + ", address=" + address
+				+ ", nationality=" + nationality + "]";
+	} //end of toString
 	
-	
-}
+} //end of Tenant Class
