055b94c0ad01bbd2d59160cb1ac51e6f4f9e8db7
MahnoorAkhund <62069409+MahnoorAkhund@users.noreply.github.com>
2020-03-11 19:44:01 +0300

    Update Visitor.java

--- Visitor.java
+++ Visitor.java
@@ -2,4 +2,26 @@ package people;
 
 public class Visitor extends Tenant {
 
+  private int passportNo;
+	private Date entryDate;
+	private Date visaExpiryDate;
+	
+	public int getPassportNo() {
+		return passportNo;
+	}
+	public void setPassportNo(int passportNo) {
+		this.passportNo = passportNo;
+	}
+	public Date getEntryDate() {
+		return entryDate;
+	}
+	public void setEntryDate(Date entryDate) {
+		this.entryDate = entryDate;
+	}
+	public Date getVisaExpiryDate() {
+		return visaExpiryDate;
+	}
+	public void setVisaExpiryDate(Date visaExpiryDate) {
+		this.visaExpiryDate = visaExpiryDate;}
+  
 }
