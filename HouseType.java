104ae583438277453da30dde61af691e96b525c2
MahnoorAkhund <62069409+MahnoorAkhund@users.noreply.github.com>
2020-03-12 21:57:45 +0300

    Update HouseType.java

--- HouseType.java
+++ HouseType.java
@@ -1,49 +1,45 @@
 package projectphase1;
 
-public class HouseType {
+public class HouseType { // Created by Malak Nabeel
 
 	private int typeId;
 	private String description;
 	private double housePrice;
-	
-	
+
 	public HouseType(int typeId, String description, double housePrice) {
 		super();
 		this.typeId = typeId;
 		this.description = description;
 		this.housePrice = housePrice;
-	}
+	}//end of HouseType constructor
 
 	public int getTypeId() {
 		return typeId;
-	}
-	
+	}//end of getTypeId method
+
 	public void setTypeId(int typeId) {
 		this.typeId = typeId;
-	}
-	
+	}//end of setTypeId method
+
 	public String getDescription() {
 		return description;
-	}
-	
+	}//end of getDescription method
+
 	public void setDescription(String description) {
 		this.description = description;
-	}
-	
+	}//end of setDescription method
+
 	public double getHousePrice() {
 		return housePrice;
-	}
-	
+	}//end of getHousePrice method
+
 	public void setHousePrice(double housePrice) {
 		this.housePrice = housePrice;
-	}
+	}//end of setHousePrice method
 
 	@Override
 	public String toString() {
 		return "HouseType [typeId=" + typeId + ", description=" + description + ", housePrice=" + housePrice + "]";
-	}
-	
-	
-	
-	
+	}//end of toString method
+
 }//end of HouseType class
